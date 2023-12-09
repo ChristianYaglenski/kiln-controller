bba52cacff6244b4e6132fae872ecc3b599e7943
Christian Yaglenski <yags@yagsbags.dev>
2023-12-08 23:00:44 -0500

    add setup script

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get dist-upgrade -y
+sudo apt-get install python3-dev python3-virtualenv libevent-dev virtualenv -y
+git clone https://github.com/ChristianYaglenski/kiln-controller
+cd kiln-controller
+virtualenv -p python3 venv
+source venv/bin/activate
+export CFLAGS=-fcommon
+pip3 install --upgrade setuptools
+pip3 install greenlet bottle gevent gevent-websocket
+cd kiln-controller
+virtualenv -p python3 venv
+source venv/bin/activate
+export CFLAGS=-fcommon
+pip3 install -r requirements.txt
+/home/pi/kiln-controller/start-on-boot
+source venv/bin/activate
+./kiln-controller.py
